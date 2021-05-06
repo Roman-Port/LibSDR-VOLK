682388b50cef4b932d92ca270367018f217f8b77
Roman Port <34401219+Roman-Port@users.noreply.github.com>
2021-05-05 23:26:35 -0500

    Install mako

--- libsdr/setup.sh
+++ libsdr/setup.sh
@@ -2,6 +2,9 @@
 sudo apt update
 sudo apt install build-essential cmake python3.8 python3-pip mingw-w64 mingw-w64-common -y
 
+# Download mako
+pip3 install mako
+
 # Obtain the NDK
 wget https://dl.google.com/android/repository/android-ndk-r21e-linux-x86_64.zip
 
