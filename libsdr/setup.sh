d6363fcce7da688479f81fdc36b513ae8c7b5913
Roman Port <34401219+Roman-Port@users.noreply.github.com>
2021-05-05 21:10:03 -0500

    Made setup.sh apt install all dependencies

--- libsdr/setup.sh
+++ libsdr/setup.sh
@@ -1,3 +1,7 @@
+# Download the required dependencies
+sudo apt update
+sudo apt install build-essential cmake python3.8 python3-pip mingw-w64 mingw-w64-common -y
+
 # Obtain the NDK
 wget https://dl.google.com/android/repository/android-ndk-r21e-linux-x86_64.zip
 
@@ -6,6 +10,3 @@ unzip -q android-ndk-r21e-linux-x86_64.zip
 
 # Now that it's unpacked, remove the ZIP
 rm android-ndk-r21e-linux-x86_64.zip
-
-# Install mingw for Windows builds
-sudo apt-get install mingw-w64
